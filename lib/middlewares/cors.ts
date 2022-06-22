edabfb6322327d217683b4e7d28c29c767d79e64
Maximo <maximorossini2016@gmail.com>
2022-06-22 20:54:57 -0300

    fighting agains CORS

--- lib/middlewares/cors.ts
+++ lib/middlewares/cors.ts
@@ -17,5 +17,6 @@ export const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
   Cors({
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    origin: "*",
   })
 );
